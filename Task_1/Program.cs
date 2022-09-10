3439eaa685bd964db8c7cc470210008cded87500
PolinaG <PolinaG@gmail.com>
2022-09-10 17:01:11 +0300

    new commit

---
+++ Task_1/Program.cs
@@ -0,0 +1,20 @@
+﻿int PositiveNumbers()
+{
+System.Console.WriteLine("Введите числа: ");
+int count = 0;
+for(int i = 0; ; i++)
+{
+    string a = Console.ReadLine();
+    if(a=="stop")
+    break;
+    else
+{
+    int b = Convert.ToInt32(a);
+    if(b>0)
+    count++;
+}
+}
+System.Console.WriteLine($"кол-во положительных чисел = {count}");
+return count;
+}
+PositiveNumbers();
---
+++ Task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿System.Console.WriteLine("Введите числа через пробел: ");
+string[] stringArray = Console.ReadLine().Split(' ');
+double b1 = double.Parse(stringArray[0]);
+double k1 = double.Parse(stringArray[1]);
+double b2 = double.Parse(stringArray[2]);
+double k2 = double.Parse(stringArray[3]);
+
+
+double[] Point(double b1, double b2, double k1, double k2)
+{
+    
+    double[] result = new double[2];
+    result[0] = (b2 - b1)/(k1 - k2);
+    result[1] = k1 * result[0]+b1;
+
+    return result;
+}
+System.Console.WriteLine(string.Join(" ", Point(b1, b2, k1, k2)));
